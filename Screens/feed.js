99ecd61fe4db159bfcdebbf056acb9f577927476
RajasMahajan <73475718+RajasMahajan@users.noreply.github.com>
2021-08-21 21:00:54 +0530

    Add files via upload

---
+++ Screens/feed.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Deeds extends React.Component{
+  render()
+  {return (
+    <View >
+      <Text>feed.js</Text>
+</View>
+  );
+}
+}
